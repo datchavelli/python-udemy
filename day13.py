6238fa02d9ad57141451a1fa4b65747a1b6d8c4b
danilo <danilo@myoffice.rs>
2025-09-08 15:20:32 +0200

    Dan 13. Debug

---
+++ day13.py
@@ -0,0 +1,12 @@
+# Dan 13 je bio debugging. Objasnjavanje kako se radi debugging.
+# 1. Opisati problem
+# 2. Reprodukcija greske
+# 3. Razmisljaj kao racunar (kako interpretira kod)
+# 4. Ispravi vidljive greske
+# 5. Koristi print() f-ju
+# 6. Koristi Debugger 
+# 7. Napravi pauzu
+# 8. Pitaj prijatelja
+# 9. Cesto pokreci kod (ne cekaj da iskucas sve)
+# 10. Stack Overflow / ChatGPT 
+# FizzBuzz game za proveru. 
